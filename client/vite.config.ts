2eda9574b866a47a9548f08d7c256308065946ff
Anton Kurtin <norflin321@gmail.com>
2025-04-15 13:33:58 +0300

    chore: setup vite typescript checker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,24 @@
+import checker from "vite-plugin-checker";
 import { defineConfig } from "vite"
 import preact from "@preact/preset-vite"
 import path from "path"
 
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		checker({ typescript: { root: "./client", tsconfigPath: path.resolve(__dirname, "./tsconfig.client.json"), }, overlay: { initialIsOpen: false } }),
+		preact(),
+	],
+
 	root: path.resolve(__dirname),
 	resolve: {
 		alias: { "@": path.resolve(__dirname, "..") },
-	}
+	},
+
+	build: {
+		outDir: "build",
+		emptyOutDir: true,
+		rollupOptions: {
+			input: path.resolve(__dirname, "index.html"),
+		},
+	},
 })
